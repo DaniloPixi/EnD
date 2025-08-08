4ddb85bc7b58cc451b4e63c97920c0ca080cab80
Pixi10 <noxus010@gmail.com>
2025-08-08 15:30:16 +0200

    Another vite.config change

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,43 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from 'vite-plugin-pwa'
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg','pwa-192x192.png','pwa-512x512.png', '180.png'],
-      manifest: {
-        name: 'GruS Corner',
-        short_name: 'GruS Corner',
-        description: 'A shared space for memos and plans',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '180.png',
-            sizes: '180x180',
-            type: 'image/png',
-            purpose: 'any'
-          }
-        ],
-    
-      },
-    })
-  ],
-  base: '/EnD/',
-  build: {
-    outDir: 'dist',
-  }
+  base: '/EnD/',
+  plugins: [
+    vue(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg','pwa-192x192.png','pwa-512x512.png', '180.png'],
+      manifest: {
+        name: 'GruS Corner',
+        short_name: 'GruS Corner',
+        description: 'A shared space for memos and plans',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '180.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ],
+      },
+    })
+  ],
+  
+  build: {
+    outDir: 'dist',
+  }
 });
